09d99f45f71cce2a8327436d1ab2a51981797b88
Apollo Tang <tang.apollo@gmail.com>
2017-04-29 12:48:47 -0400

    delete now delete user from users resources

--- src/modules/user-review/reducers/resources/users.js
+++ src/modules/user-review/reducers/resources/users.js
@@ -20,37 +20,29 @@ const users = (state = {}, action) => {
         // payload receive a collection of users object in array
         usersFromPayload=payload.users;
       }
-
-      console.log('yyyyyyy usersFromPayload; ', usersFromPayload);
       const state_prev = state;
-      console.log('yyyyyyy state; ', state);
       const newUserObj = array_to_IndexirizedObj(usersFromPayload)
-      console.log('yyyyyyy newUserObj; ', newUserObj);
       const state_next = {
         ...state_prev,
         ...newUserObj
       };
-      console.log('yyyyyyy state_next; ', state_next);
       return state_next;
     }
     case c[`${nameSpace}__resources_users_delete`] : {
-      console.log('rrrr in reduce resources'  )
       const payload = action.payload
 
       let id_deletedUser;
-      console.log('rrrr in reduce resources users: payload: ', payload )
       if (payload.hasOwnProperty('deletedUser')) {
         // payload receive a single user object
         id_deletedUser = payload.deletedUser._id;
-        console.log('rrrr id_deletedUser: ', id_deletedUser)
       }
       if (payload.hasOwnProperty('deletedUsers')) {
         // payload receive a collection of users object in array
         // ... not implimented
       }
 
-      console.log('state; ', state);
-      const users_next = _.cloneDeep(state.users);
+      const state_prev = state;
+      const users_next = _.cloneDeep(state);
       delete users_next[id_deletedUser]
       return users_next;
     }
