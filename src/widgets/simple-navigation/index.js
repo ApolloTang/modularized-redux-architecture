01ba0797aa954866e38abcc0799885171abd1024
Apollo Tang <tang.apollo@gmail.com>
2017-04-30 21:41:28 -0400

    (wip) testing simple-navigation widget

--- src/widgets/simple-navigation/index.js
+++ src/widgets/simple-navigation/index.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-import style from './style';
+
+import style from './style.less';
 const ModuleRoot = ({navigations}) => (
-    <div className={`simple-navigation ${style['module-style']}`}>
-        <ul>
-            {
-                navigations.map((n,i)=>(
-                    <li key={i}>
-                        <NavLink
-                            exact
-                            to={n.to}
-                            activeClassName="is-active" >
-                            {n.displayText}
-                        </NavLink>
-                    </li>
-                ))
-            }
-        </ul>
-    </div>
-)
+  <div className={`simple-navigation ${style['module-style']}`}>
+    <ul>
+      {
+        navigations.map((n,i)=>(
+          <li key={i}>
+            <NavLink
+              exact
+              to={n.to}
+              activeClassName="is-active" >
+              {n.displayText}
+            </NavLink>
+          </li>
+        ))
+      }
+    </ul>
+  </div>
+);
+ModuleRoot.propTypes = {
+  navigations: PropTypes.array
+};
+
 export default ModuleRoot;
 
 
