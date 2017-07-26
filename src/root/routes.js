66a004fce4321793d337776dd9be327a6fb6fb22
Apollo Tang <tang.apollo@gmail.com>
2017-07-26 14:28:04 -0400

    [demo-01] for demotrating modularied redux architecture

--- src/root/routes.js
+++ src/root/routes.js
@@ -13,12 +13,12 @@ import Playground from 'modules/playground';
 
 const navigationDirective = [
     {to:'/', displayText:'Home'},
-    {to:'/users', displayText:'User review'},
+    {to:'/todos/all', displayText:'Todo'},
     {to:'/font-test', displayText:'Font test'},
     {to:'/lazy-load', displayText:'Lazy load'},
-    {to:'/todos/all', displayText:'Todo'},
-    {to:'/rest', displayText:'Rest'},
     {to:'/playground', displayText:'playground'}
+    // {to:'/users', displayText:'User review'},
+    // {to:'/rest', displayText:'Rest'},
 ];
 
 
@@ -26,16 +26,16 @@ const navigationDirective = [
 const routes = (
   <Switch>
     <Route exact path="/" component={()=>(<div>home</div>)} />
-    {/* <Route exact path="/" render={()=>( <Redirect to="/todos/all"/>)} /> */}
-    {/* <Route exact path="/" render={()=>( <Redirect to="/users"/>)} /> */}
-    <Route path="/users/:userId" component={UserReview} />
-    <Route exact path="/users/new" component={UserReview} />
-    <Route path="/users" component={UserReview} />
     <Route path="/font-test" component={FontTest} />
     <Route path="/lazy-load" component={LazyLoad} />
     <Route path="/todos/:filterType" component={Todos} />
-    <Route path="/rest" component={Rest} />
     <Route path="/playground" component={Playground} />
+    {/* <Route exact path="/" render={()=>( <Redirect to="/todos/all"/>)} /> */}
+    {/* <Route exact path="/" render={()=>( <Redirect to="/users"/>)} /> */}
+    {/* <Route path="/users/:userId" component={UserReview} /> */}
+    {/* <Route exact path="/users/new" component={UserReview} /> */}
+    {/* <Route path="/users" component={UserReview} /> */}
+    {/* <Route path="/rest" component={Rest} /> */}
     <Route component={NotFound}/>
   </Switch>
 );
